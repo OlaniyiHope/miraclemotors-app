daf841be6bfde58612d95f9794ae5690e6d0dcdc
thecodearcher <brian.iyoha@gmail.com>
2020-02-16 10:19:49 +0100

    feat: implement passengers count feature

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Helpers } from './../../app.helpers';
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor(private helpers: Helpers) { }
+  passengers = { adults: 1, children: 0, total: 1 };
+  constructor(private helpers: Helpers, private alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
 
-  async openTerminalSearch() {
+  async openTerminalSearch(type: 'departure' | 'arrival') {
     this.helpers.navPush('/terminal-search');
+  }
+
+  async openPassengers() {
+    const alert = await this.alertCtrl.create({
+      header: 'Number of Travelers',
+      inputs: [
+        {
+          name: 'adults',
+          type: 'number',
+          placeholder: 'Number of Adult Travelers',
+          min: 1,
+          value: this.passengers.adults
+        },
+        {
+          name: 'children',
+          type: 'number',
+          label: 'Children Travelers',
+          placeholder: 'Number of Children Travelers',
+          value: this.passengers.children
+        },
+
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+        }, {
+          text: 'Ok',
+        }
+      ]
+    });
+
+    await alert.present();
+    const { values: { adults, children } } = (await alert.onDidDismiss()).data;
+    this.passengers.adults = (!adults || adults == 0) ? 1 : adults;
+    this.passengers.children = children;
+    this.passengers.total = Number(this.passengers.adults) + Number(this.passengers.children);
+    console.log(adults, children);
+
   }
 }
